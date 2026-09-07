75af366394a620127511d3d5c3c0029a2d40cbaa
tclwasherexd-star <tclwasherexd@gmail.com>
2026-09-07 14:20:48 +0800

    Initialize package.json for Minecraft offline AFK

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "minecraft-offline-afk",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "mineflayer": "^4.20.0",
+    "express": "^4.19.0"
+  },
+  "scripts": {
+    "start": "node index.js"
+  }
+}
+
